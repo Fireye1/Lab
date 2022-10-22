630fdc34a423ac0361fd7facdfdb8a92130cad01
Valeh <114465476+VaLeh5@users.noreply.github.com>
2022-10-22 19:19:58 +0400

    Add files via upload

---
+++ Valeh.js
@@ -0,0 +1,9 @@
+function div2(array) {
+    for (let i=0; i<array.length; i++) {
+    if (array[i]%2 ==0) {
+    console.log(array[i]);
+    }
+    }
+    }
+    const sample_array = [1,2,4,6,71,2,4,9,100];
+    div2(sample_array);
